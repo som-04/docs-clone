65d9a06eb5b4ebd11a3370e07905f83ccfaab33b
som-04 <somyasati14@gmail.com>
2025-08-11 12:59:27 +0300

    added: page-like ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 const Home = () => {
   return (
-    <div>
-      sup
+    <div className="flex min-h-screen items-center justify-center">
+      click
+      <Link href="/documents/123">
+        <span className="text-blue-500">&nbsp;here&nbsp;</span>
+      </Link>
+      s to go to document id
     </div>
-  )
-}
+  );
+};
 
 export default Home;
