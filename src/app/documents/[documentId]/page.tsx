16492eda2aa92709a3e8ce2a8d82eec3117665b6
som-04 <somyasati14@gmail.com>
2025-08-11 17:08:46 +0300

    init: toolbar

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,14 +1,15 @@
 import { Editor } from "./editor";
-
+import { Toolbar } from "./toolbar";
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
 }
 
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
-  const {documentId} = await params;
-
+  const { documentId } = await params;
+  
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
+      <Toolbar />
       <Editor />
     </div>
   );
