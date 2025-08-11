0b0e76361dd2567b6eb2e8b7792488608e68a940
som-04 <somyasati14@gmail.com>
2025-08-11 14:52:26 +0300

    fixed: lists

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,18 +1,26 @@
 "use client";
 
 import { useEditor, EditorContent } from "@tiptap/react";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
 import StarterKit from "@tiptap/starter-kit";
 
 export const Editor = () => {
   const editor = useEditor({
     editorProps: {
       attributes: {
-        style:"padding-left: 56px; padding-right: 56px;",
+        style: "padding-left: 56px; padding-right: 56px;",
         class:
           "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+    ],
     content: "<p>Hello World!</p>",
   });
 
